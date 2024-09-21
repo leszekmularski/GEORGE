b35657bc726b8945f96ea6ec0155f6eaf0d9c0b4
Leszek Mularski <leszek.mularski@george.pl>
2024-09-21 20:22:20 +0200

    P-21-09-2024/1

---
+++ Server/Migrations/20240921181953_InitialMigration34.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GEORGE.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration34 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NumerZlecenia",
+                table: "ZleceniaProdukcyjneWew",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NumerZlecenia",
+                table: "ZleceniaProdukcyjne",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumerZlecenia",
+                table: "ZleceniaProdukcyjneWew");
+
+            migrationBuilder.DropColumn(
+                name: "NumerZlecenia",
+                table: "ZleceniaProdukcyjne");
+        }
+    }
+}
