ed3d3383ac45b2aaf544a04bfda05a4006176293
Leszek Mularski <leszek.mularski@george.pl>
2024-09-26 14:32:08 +0200

    P-06-09-2024/2

---
+++ Server/Controllers/KartaInstrSystemController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GEORGE.Server.Controllers;
+using GEORGE.Shared.Models;
+using System.Net;
+
+namespace GEORGE.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KartaInstrSystemController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public KartaInstrSystemController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<KartyInstrukcyjne>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<List<KartyInstrukcyjne>>> ListAsync()
+        {
+
+            try
+            {
+
+                var produkt = await context.KartyInstrukcyjne
+                .Where(p => p.KodProduktu != null)
+                .Select(p => p.KodProduktu)
+                .Distinct()
+                .ToListAsync();
+
+                return Ok(produkt);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return NotFound();
+            }
+
+        }
+    }
+}
