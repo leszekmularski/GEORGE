efe1adf69d9e459c539e6d702b4890efffe5804b
Leszek Mularski <leszek.mularski@george.pl>
2024-11-25 13:53:55 +0100

    P-25-11-2024/1

--- Client/Pages/PDF/Zestawienie.cs
+++ Client/Pages/PDF/Zestawienie.cs
@@ -185,5 +185,52 @@ namespace GEORGE.Client.Pages.PDF
             return zaokraglonaDlugosc.ToString();
         }
 
+        public Zestawienie ParseClipboardData(string clipboardText)
+        {
+            var zestawienie = new Zestawienie();
+            var lines = clipboardText.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var line in lines.Skip(2)) // Pomijamy pierwsze dwie linie (Data/Godzina i nagłówki)
+            {
+                var items = line.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (items.Length >= 9) // Zakładamy, że linia musi mieć co najmniej 10 elementów
+                {
+                    try
+                    {
+                        var cutItem = new CutListItem
+                        {
+                            Ilosc = items[0].Trim(),
+                            Wymiar = items[items.Count() - 1].Trim(),
+                            Kat = items[1].Trim(),
+                            WymiarNaZamowienie = DlugoscHandlowa(items[5]),
+                            Lp = items[6].Trim(),
+                            Uwagi = line//items[9].Trim() // Przyjmuję, że ostatni element jest opisem, ale możesz to dostosować
+                        };
+
+                        zestawienie.ListaCieci.Add(cutItem);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Błąd w parsowaniu linii: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Za mało elementów w linii: {line}");
+                }
+            }
+
+
+            //cutListItem.Ilosc = items[4];
+            //cutListItem.Wymiar = items[5];
+            //cutListItem.Kat = (items.Length > 6 ? items[6] : "");
+            //cutListItem.WymiarNaZamowienie = DlugoscHandlowa(items[5]);
+            //cutListItem.Uwagi = strUwagi.Substring(0, strUwagi.IndexOf("-")).TrimEnd();
+
+            Console.WriteLine($"ZestawienieSz ListaSzyb.Count: {zestawienie.ListaCieci.Count()}");
+            return zestawienie;
+        }
+
     }
 }
