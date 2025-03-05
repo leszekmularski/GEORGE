3a95eb525f606110b03464f455a37a7f96b4e2a4
Leszek Mularski <leszek.mularski@george.pl>
2025-03-05 14:45:58 +0100

    P-05-03-2025/5

--- Server/Controllers/KonfModeleElementyController.cs
+++ Server/Controllers/KonfModeleElementyController.cs
@@ -15,24 +15,31 @@ public class KonfModeleElementyController : ControllerBase
         _context = context;
     }
 
-
     [HttpGet("powiazaniajuzzaznaczone/{rowIdKonfModele}")]
     public async Task<ActionResult<MVCKonfModele>> GetPowiazaniajuzaznaczone(Guid rowIdKonfModele)
     {
+        // Pobranie modelu z KonfModele
         var model = await _context.KonfModele
             .FirstOrDefaultAsync(m => m.RowId == rowIdKonfModele);
 
+        // Pobranie powiązanych elementów z KonfModeleElementy
         var powiazaneElementy = await _context.KonfModeleElementy
             .Where(k => k.RowIdKonfModele == rowIdKonfModele)
             .ToListAsync();
 
+        // Pobranie RowIdKonfModele z powiązanych elementów
+        var rowIdsKonfModele = powiazaneElementy.Select(k => k.RowIdElement).ToList();
+
+        // Pobranie wszystkich KonfSystem, które pasują do RowIdKonfModele
         var konfSystem = await _context.KonfSystem
-            .Where(k => k.RowId == rowIdKonfModele)  // Zmodyfikuj w zależności od zależności w bazie
+            .Where(k => rowIdsKonfModele.Contains(k.RowId)) // Sprawdzamy, czy RowId KonfSystem znajduje się w rowIdsKonfModele
             .ToListAsync();
 
+        // Sprawdzamy, czy model istnieje
         if (model == null)
             return NotFound();
 
+        // Zwracamy obiekt MVCKonfModele z odpowiednimi danymi
         return new MVCKonfModele
         {
             KonfModele = new List<KonfModele> { model }, // Pojedynczy obiekt zamieniony na listę
