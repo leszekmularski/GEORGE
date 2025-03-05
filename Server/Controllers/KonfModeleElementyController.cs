a0d65228c760bd454e884e2e8cf3ef0262b1e5bd
Leszek Mularski <leszek.mularski@george.pl>
2025-03-05 14:00:17 +0100

    P-05-03-2025/4

--- Server/Controllers/KonfModeleElementyController.cs
+++ Server/Controllers/KonfModeleElementyController.cs
@@ -1,5 +1,6 @@
 ﻿using GEORGE.Server;
 using GEORGE.Shared.Models;
+using GEORGE.Shared.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,12 +16,31 @@ public class KonfModeleElementyController : ControllerBase
     }
 
 
-    [HttpGet("powiazania/{rowIdKonfModele}")]
-    public async Task<ActionResult<List<KonfModeleElementy>>> GetPowiazania(Guid rowIdKonfModele)
+    [HttpGet("powiazaniajuzzaznaczone/{rowIdKonfModele}")]
+    public async Task<ActionResult<MVCKonfModele>> GetPowiazaniajuzaznaczone(Guid rowIdKonfModele)
     {
-        return await _context.KonfModeleElementy
-            .Where(k => k.RowIdKonfModele == rowIdKonfModele).ToListAsync();
+        var model = await _context.KonfModele
+            .FirstOrDefaultAsync(m => m.RowId == rowIdKonfModele);
+
+        var powiazaneElementy = await _context.KonfModeleElementy
+            .Where(k => k.RowIdKonfModele == rowIdKonfModele)
+            .ToListAsync();
+
+        var konfSystem = await _context.KonfSystem
+            .Where(k => k.RowId == rowIdKonfModele)  // Zmodyfikuj w zależności od zależności w bazie
+            .ToListAsync();
+
+        if (model == null)
+            return NotFound();
+
+        return new MVCKonfModele
+        {
+            KonfModele = new List<KonfModele> { model }, // Pojedynczy obiekt zamieniony na listę
+            KonfModeleElementy = powiazaneElementy,
+            KonfSystem = konfSystem, // Dodajemy listę KonfSystem
+        };
     }
+
 
     [HttpPost("powiazania")]
     public async Task<IActionResult> DodajPowiazania(List<KonfModeleElementy> powiazania)
---
+++ Shared/ViewModels/MVCKonfModele.cs
@@ -0,0 +1,13 @@
+﻿using GEORGE.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MVCKonfModele
+{
+    public List<KonfModele>? KonfModele { get; set; }
+    public List<KonfModeleElementy>? KonfModeleElementy { get; set; }
+    public List<KonfSystem>? KonfSystem { get; set; }
+}
