f8cd21d1b62117a84037af4074df8da41f6027ec
Leszek Mularski <leszek.mularski@george.pl>
2025-03-08 07:31:51 +0100

    P-08-03-2025/1

---
+++ Server/Migrations/20250308063055_InitialMigration55.cs
@@ -0,0 +1,252 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GEORGE.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration55 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "PoziomOsDormas",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PoziomLiniaSzkla",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PoziomLiniaOkucia",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PoziomKorpus",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PoziomGora",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PoziomDol",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PoziomDodatkowa7",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PoziomDodatkowa6",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PionPrawa",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PionOdSzybaOdZew",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PionLewa",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PionDodatkowa5",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PionDodatkowa4",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PoziomOsDormas",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PoziomLiniaSzkla",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PoziomLiniaOkucia",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PoziomKorpus",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PoziomGora",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PoziomDol",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PoziomDodatkowa7",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PoziomDodatkowa6",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PionPrawa",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PionOdSzybaOdZew",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PionLewa",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PionDodatkowa5",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PionDodatkowa4",
+                table: "KonfSystem",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+        }
+    }
+}
