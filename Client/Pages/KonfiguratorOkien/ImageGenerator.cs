d0699969a007b3a53584656f15a72a26e7b4f598
lesmul <lesmul@wp.pl>
2025-03-12 20:26:28 +0100

    D-12-03-2025/1

--- Client/Pages/KonfiguratorOkien/ImageGenerator.cs
+++ Client/Pages/KonfiguratorOkien/ImageGenerator.cs
@@ -73,56 +73,66 @@ public class ImageGenerator
                 var texture = woodTexture.Clone(tx => tx.Crop(new Rectangle(0, 0, width, height)));
                 orderedFrames.Add((frame, texture, new Point(x, y)));
             }
-            //EdytowanyModel.PolaczenieNaroza: 180-T1;90-T1;0-T1;270-T1 gdzie 180-90 poziomy górny | 90-0 prawy pionowy | 90-270 - poziomy dolny | 270-180 - lewy pionowy
-            //EdytowanyModel.PolaczenieNaroza: 180-T2;90-T1;0-T1;270-T1
 
-            if (polaczeniaArray[0].typ == "T1" && polaczeniaArray[1].typ == "T1")
+            // 🔥 Kolejność rysowania: najpierw T3, ale pionowe T3 (90°, 270°) przed poziomymi (0°, 180°)
+            var drawOrder = new List<(int index, int angle, string type)>
             {
+                (0, polaczeniaArray[0].kat, polaczeniaArray[0].typ), // Góra
+                (1, polaczeniaArray[1].kat, polaczeniaArray[1].typ), // Prawa
+                (2, polaczeniaArray[2].kat, polaczeniaArray[2].typ), // Dół
+                (3, polaczeniaArray[3].kat, polaczeniaArray[3].typ)  // Lewa
+            };
 
-                // Generowanie elementów ramy z odpowiednimi połączeniami
-                AddFrame(0, 0, (int)profileLeftWidth, imageSize, polaczeniaArray[3].typ); // Lewa
-                AddFrame(imageSize - (int)profileRightWidth, 0, (int)profileRightWidth, imageSize, polaczeniaArray[1].typ); // Prawa
-                AddFrame(0, 0, imageSize, (int)profileTopWidth, polaczeniaArray[0].typ); // Góra
-                AddFrame(0, imageSize - (int)profileBottomWidth, imageSize, (int)profileBottomWidth, polaczeniaArray[2].typ); // Dół
+            drawOrder = drawOrder
+                .OrderBy(p => p.type == "T1" ? 0 : 1) // T3 jako pierwsze
+                .ThenBy(p => p.type == "T1" ? (p.angle == 90 || p.angle == 270 ? 0 : 1) : 2) // Pionowe T3 przed poziomymi T3
+                .ToList();
+
+            foreach (var (index, angle, typ) in drawOrder)
+            {
+                Console.WriteLine($"index: {index}, angle: {angle}, typ: {typ}");
+
+                switch (index)
+                {
+                    case 0: // GÓRA
+                        AddFrame(0, 0, imageSize, (int)profileTopWidth, typ);
+                        break;
+                    case 1: // PRAWA
+                        AddFrame(imageSize - (int)profileRightWidth, 0, (int)profileRightWidth, imageSize, typ);
+                        break;
+                    case 2: // DÓŁ
+                        AddFrame(0, imageSize - (int)profileBottomWidth, imageSize, (int)profileBottomWidth, typ);
+                        break;
+                    case 3: // LEWA
+                        AddFrame(0, 0, (int)profileLeftWidth, imageSize, typ);
+                        break;
+                }
             }
 
-            if (polaczeniaArray[0].typ == "T3" && polaczeniaArray[1].typ == "T3")
-            {
-                // Generowanie elementów ramy z odpowiednimi połączeniami
-                AddFrame(0, 0, imageSize, (int)profileTopWidth, polaczeniaArray[0].typ); // Góra
-                AddFrame(0, imageSize - (int)profileBottomWidth, imageSize, (int)profileBottomWidth, polaczeniaArray[2].typ); // Dół
-                AddFrame(imageSize - (int)profileRightWidth, 0, (int)profileRightWidth, imageSize, polaczeniaArray[1].typ); // Prawa
-                AddFrame(0, 0, (int)profileLeftWidth, imageSize, polaczeniaArray[3].typ); // Lewa;
-            }
-
-            if (polaczeniaArray[0].typ == "T1" && polaczeniaArray[1].typ == "T3")
-            {
-                // Generowanie elementów ramy z odpowiednimi połączeniami
-                AddFrame(0, 0, imageSize, (int)profileTopWidth, polaczeniaArray[0].typ); // Góra
-                AddFrame(imageSize - (int)profileRightWidth, 0, (int)profileRightWidth, imageSize, polaczeniaArray[1].typ); // Prawa
-                AddFrame(0, imageSize - (int)profileBottomWidth, imageSize, (int)profileBottomWidth, polaczeniaArray[2].typ); // Dół
-                AddFrame(0, 0, (int)profileLeftWidth, imageSize, polaczeniaArray[3].typ); // Lewa
-            }
-            if (polaczeniaArray[0].typ == "T3" && polaczeniaArray[1].typ == "T1")
-            {
-                // Generowanie elementów ramy z odpowiednimi połączeniami
-                AddFrame(0, 0, imageSize, (int)profileTopWidth, polaczeniaArray[0].typ); // Góra
-                AddFrame(imageSize - (int)profileRightWidth, 0, (int)profileRightWidth, imageSize, polaczeniaArray[1].typ); // Prawa
-                AddFrame(0, imageSize - (int)profileBottomWidth, imageSize, (int)profileBottomWidth, polaczeniaArray[2].typ); // Dół
-                AddFrame(0, 0, (int)profileLeftWidth, imageSize, polaczeniaArray[3].typ); // Lewa
-            }
-
+            // 🔥 Obsługa narożników T2 (45°)
             if (polaczeniaArray.Any(p => p.typ == "T2"))
             {
                 orderedFrames.Clear();
-                orderedFrames.Add((new Polygon(new PointF[] {
+                orderedFrames.Add((new Polygon(new PointF[]
+                {
+                    new PointF(imageSize - (float)profileRightWidth, 0),
+                    new PointF(imageSize, (float)profileTopWidth),
                     new PointF(0, (float)profileTopWidth),
                     new PointF((float)profileLeftWidth, 0),
-                    new PointF(imageSize - (float)profileRightWidth, 0),
-                    new PointF(imageSize, (float)profileTopWidth)
+           
                 }), woodTexture.Clone(), new Point(0, 0)));
+
+                orderedFrames.Add((new Polygon(new PointF[]
+                {
+                    new PointF(imageSize - (float)profileRightWidth, imageSize),
+                    new PointF(imageSize, imageSize - (float)profileBottomWidth),
+                    new PointF(0, imageSize - (float)profileBottomWidth),
+                    new PointF((float)profileLeftWidth, imageSize),
+                    
+                }), woodTexture.Clone(), new Point(0, (int)profileTopWidth)));
             }
 
+            // 🔥 Tworzenie szyby w środku
             IPath glassPath = new RectangularPolygon(
                 (int)profileLeftWidth,
                 (int)profileTopWidth,
@@ -130,6 +140,7 @@ public class ImageGenerator
                 imageSize - (int)profileTopWidth - (int)profileBottomWidth
             );
 
+            // 🔥 Rysowanie ramy okna na obrazie
             image.Mutate(x =>
             {
                 foreach (var (path, texture, position) in orderedFrames)
@@ -139,6 +150,7 @@ public class ImageGenerator
                     x.Draw(Pens.Solid(Color.Black, borderThickness), path);
                 }
 
+                // 🔥 Dodanie szyby (jasnoniebieskie wypełnienie)
                 x.Fill(Color.LightBlue.WithAlpha(0.4f), glassPath);
                 x.Draw(Pens.Solid(Color.Black, borderThickness), glassPath);
             });
