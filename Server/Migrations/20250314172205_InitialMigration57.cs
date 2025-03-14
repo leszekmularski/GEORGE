1b5a93fada48c053fb572f23cde7650fb6dede7f
Leszek Mularski <leszek.mularski@george.pl>
2025-03-14 18:23:01 +0100

    P-14-03-2025/5

---
+++ Server/Migrations/20250314172205_InitialMigration57.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GEORGE.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration57 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "PionOsSymetrii",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "PoziomOsSymetrii",
+                table: "KonfSystem",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PionOsSymetrii",
+                table: "KonfSystem");
+
+            migrationBuilder.DropColumn(
+                name: "PoziomOsSymetrii",
+                table: "KonfSystem");
+        }
+    }
+}
