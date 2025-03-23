90c2df42748bf586d8a67bd063f65f45dc459379
lesmul <lesmul@wp.pl>
2025-03-23 18:40:57 +0100

    D-23-03-2025/2-Wersja rozowoja

---
+++ Client/Pages/KonfiguratorOkien/WygenerowaneMaterialy.cs
@@ -0,0 +1,20 @@
+﻿namespace GEORGE.Client.Pages.KonfiguratorOkien
+{
+    public class WygenerowaneMaterialy
+    {
+        public double Dlugosc { get; set; }
+        public string Nazwa { get; set; }
+        public string Indeks { get; set; }
+        public float IloscSztuk { get; set; }
+        public string Restrykcja { get; set; }
+
+        public WygenerowaneMaterialy(double dlugosc, string nazwa, string indeks, float iloscSztuk, string restrykcja)
+        {
+            Dlugosc = dlugosc;
+            Nazwa = nazwa;
+            Indeks = indeks;
+            IloscSztuk = iloscSztuk;
+            Restrykcja = restrykcja;
+        }
+    }
+}
---
+++ Client/Pages/KonfiguratorOkien/WyrobWymiaryOpis.cs
@@ -0,0 +1,18 @@
+﻿namespace GEORGE.Client.Pages.KonfiguratorOkien
+{
+    public class WyrobWymiaryOpis
+    {
+        public double Szerokosc { get; set; }
+        public double Wysokosc { get; set; }
+        public string RodzajObiektu { get; set; }
+        public string Restrykcja { get; set; }
+
+        public WyrobWymiaryOpis(double szerokosc, double wysokosc, string rodzajObiektu, string restrykcja)
+        {
+            Szerokosc = szerokosc;
+            Wysokosc = wysokosc;
+            RodzajObiektu = rodzajObiektu;
+            Restrykcja = restrykcja;
+        }
+    }
+}
